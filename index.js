09589cd6ec302be6908e19bfb2f59c2ae566ba86
Ensky Lin <linhomeyeu@gmail.com>
2014-06-21 17:49:59 +0800

    disk driver done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var conf = require('./lib/config')
+  , memLRUCache = require('./lib/lruCache').driver('memory')
+  , diskLRUCache = require('./lib/lruCache').driver('disk');
+
 var log4js = require('log4js')
   , log = log4js.getLogger()
   , crypto = require('crypto')
@@ -6,14 +10,10 @@ var log4js = require('log4js')
   , request = require('request').defaults({
             encoding: null
           , proxy: 'http://localhost:3128'
-          , forever: true
-          , timeout: 5 * 1000 // 5 seconds
+          , forever: conf.proxy.http.reuseConnection
+          , timeout: eval(conf.proxy.http.timeout)
       })
-  , proxy = connect()
-
-var conf = require('./lib/config')
-  , memLRUCache = require('./lib/lruCache').driver('memory')
-  // , diskLRUCache = require('./lib/lruCache').driver('disk')
+  , proxy = connect();
 
 var md5 = function (str) {
     return crypto.createHash('md5').update(str).digest('hex')
@@ -26,36 +26,50 @@ var initialPass = function (req, res, next) {
     req.cacheKeyHeader = req.md5 + ':h'
     req.cacheKeyBody = req.md5 + ':b'
     req.isCacheOff = req.headers['cache-control'] == 'no-cache'
+    req.hit = false;
     next()
 }
 
-var memoryHit = function (req, res, next) {
-    if (req.isCacheOff) {
-        return next()
-    }
-    log.debug('inMemory check cache: ' + req.md5)
-    memLRUCache.get(req.cacheKeyHeader, function (header) {
-        if (header === null) {
-            log.debug('MISS')
-            next()
-        } else {
-            header = JSON.parse(header)
-            memLRUCache.get(new Buffer(req.cacheKeyBody), function (body) {
-                if (body === null) {
-                    log.debug('MISS')
-                    next()
-                } else {
-                    log.debug('HIT')
-                    res.writeHead(header.statusCode, header.headers)
-                    log.info('HIT TIME: ' + (new Date().valueOf() - req.startTime))
-                    res.end(body)
-                }
-            });
+var hitCheck = function (type) { // memory or disk
+    return function (req, res, next) {
+        if (req.isCacheOff || req.hit) {
+            return next()
         }
-    });
+
+        var lruCache = type == 'memory' ? memLRUCache : diskLRUCache;
+        log.debug(type + ' check cache: ' + req.md5)
+        lruCache.get(req.cacheKeyHeader, function (header) {
+            if (header === null) {
+                log.debug(type + ' MISS')
+                next()
+            } else {
+                header = JSON.parse(header)
+                lruCache.get(new Buffer(req.cacheKeyBody), function (body) {
+                    if (body === null) {
+                        log.debug(type + ' MISS')
+                    } else {
+                        log.debug(type + ' HIT');
+                        res.writeHead(header.statusCode, header.headers);
+                        log.info(type + ' HIT TIME: ' + (new Date().valueOf() - req.startTime));
+                        res.end(body);
+
+                        // for cache into another device
+                        res.headerCache = header;
+                        res.body = body;
+                        req.hit = type;
+                    }
+                    next();
+                });
+            }
+        });
+    };
 }
 
 var fetch = function (req, res, next) {
+    if (req.hit) {
+        return next();
+    }
+
     log.info('fetch : ' + req.url);
     req.headers.connection = 'keep-alive'
     var srvReq = request({uri: req.url, headers: req.headers}, function(error, servRes, body) {
@@ -80,27 +94,28 @@ var fetch = function (req, res, next) {
     })
 }
 
-var memoryCache = function (req, res, next) {
-    if (req.isCacheOff) {
-        return next()
-    }
-    memLRUCache.set(req.cacheKeyHeader, JSON.stringify(res.headerCache))
-    memLRUCache.set(req.cacheKeyBody, res.body)
-    next()
-}
+var saveCache = function (type) {
+    return function (req, res, next) {
+        if (req.isCacheOff || req.hit == type) {
+            return next()
+        }
 
-var refreshLRU = function (req, res, next) {
-    if (req.isCacheOff) {
-        return next()
-    }
-    memLRUCache.refresh(req.cacheKeyHeader, 0)
-    memLRUCache.refresh(req.cacheKeyBody, res.body.length)
-    next()
-}
+        var lruCache = type == 'memory' ? memLRUCache : diskLRUCache;
+        lruCache.set(req.cacheKeyHeader, JSON.stringify(res.headerCache))
+        lruCache.set(req.cacheKeyBody, res.body)
+
+        lruCache.refresh(req.cacheKeyHeader, 0)
+        lruCache.refresh(req.cacheKeyBody, res.body.length)
+        next()
+    };
+};
+
+var memoryCache = 
 
 proxy.use(initialPass)
-    .use(memoryHit)
+    .use(hitCheck('memory'))
+    .use(hitCheck('disk'))
     .use(fetch)
-    .use(memoryCache)
-    .use(refreshLRU)
+    .use(saveCache('disk'))
+    .use(saveCache('memory'))
 http.createServer(proxy).listen(conf.proxy.port);
